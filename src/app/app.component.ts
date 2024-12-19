9c88ecf9c2449c3df3764302ba4d84f985f25427
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2024-12-19 14:22:35 +0200

    Setup scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     imports: [],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.scss',
 })
-export class AppComponent {
-}
+export class AppComponent {}
