b2c05a9b2307c508da2e2644ce695a30f2eb89c1
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2024-12-20 14:17:10 +0200

    Difine type for single item

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,42 +1,53 @@
 import { Component } from '@angular/core';
 
+type Department = {
+    ukName: string;
+    enName?: string;
+    link: string;
+};
+
+type DepartmentGroup = {
+    title: string;
+    items: Department[];
+};
+
 @Component({
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-    get logoPath() {
+    get logoPath(): string {
         return 'assets/footer/KNULogo.svg';
     }
 
-    socialLinks = [
+    socialLinks: { name: string; link: string }[] = [
         { name: 'instagram', link: 'assets/social networks/inst.svg' },
         { name: 'telegram', link: 'assets/social networks/tg.svg' },
     ];
 
-    departments = {
+    departments: { institutes: DepartmentGroup; faculties: DepartmentGroup } = {
         institutes: {
             title: 'ІНСТИТУТИ',
             items: [
                 {
-                    name: 'Навчально-науковий інститут "Інститут геології"',
+                    ukName: 'Навчально-науковий інститут "Інститут геології"',
                     link: 'https://geology.knu.ua/',
                 },
                 {
-                    name: 'Військовий інститут',
+                    ukName: 'Військовий інститут',
                     link: 'https://mil.knu.ua/ua/',
                 },
                 {
-                    name: 'Інститут Управління державної охорони України',
+                    ukName: 'Інститут Управління державної охорони України',
                     link: 'https://institute.do.gov.ua/',
                 },
                 {
-                    name: 'Інститут післядипломної освіти',
+                    ukName: 'Інститут післядипломної освіти',
                     link: 'https://ipe.knu.ua/',
                 },
                 {
-                    name: 'Навчально-науковий інститут',
+                    ukName: 'Навчально-науковий інститут',
                     link: 'https://philology.knu.ua/',
                 },
             ],
@@ -45,23 +56,23 @@ export class FooterComponent {
             title: 'ФАКУЛЬТЕТИ',
             items: [
                 {
-                    name: 'Географічний факультет',
+                    ukName: 'Географічний факультет',
                     link: 'https://geo.knu.ua/',
                 },
                 {
-                    name: 'Економічний факультет',
+                    ukName: 'Економічний факультет',
                     link: 'https://econom.knu.ua/',
                 },
                 {
-                    name: 'Історичний факультет',
+                    ukName: 'Історичний факультет',
                     link: 'http://history.univ.kiev.ua/',
                 },
                 {
-                    name: 'Математичний факультет',
+                    ukName: 'Математичний факультет',
                     link: 'https://mechmat.knu.ua/',
                 },
                 {
-                    name: 'Факультет інформаційних технологій',
+                    ukName: 'Факультет інформаційних технологій',
                     link: 'https://fit.knu.ua/',
                 },
             ],
