162e4a467ffbe7c78a252d73a3aee247c17dc1c6
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-01-02 15:04:50 +0200

    move routing to services

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-/// <reference types="@angular/localize" />
-
 import { provideHttpClient } from '@angular/common/http';
 import '@angular/localize/init';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideTranslateService } from '@ngx-translate/core';
 import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
+import { routes } from './app/services/app.routes';
 
 bootstrapApplication(AppComponent, {
     providers: [
