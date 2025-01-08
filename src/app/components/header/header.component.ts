ba2ae735e80532a9eaa3f7ac07da1fe218c9f595
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-01-08 19:13:56 +0200

    Fix Pull requests problem

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, HostListener, inject } from '@angular/core';
+import { Component, HostListener, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import {
     LangChangeEvent,
     TranslateModule,
@@ -22,21 +22,22 @@ type Menu = {
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
-    imports: [FormsModule, CommonModule, TranslateModule],
+    imports: [FormsModule, CommonModule, TranslateModule, RouterModule],
 })
 export class HeaderComponent {
     private i18nService = inject(I18nService);
     private translate = inject(TranslateService);
     private router = inject(Router);
     protected languageSwitcher = LanguageSwitcherService(this.translate);
-    isOpenLang = false;
-    isScrolled = false;
-    isMobile = window.innerWidth < 1024;
-    isMenuOpen = false;
+    protected currentLanguage$ = this.i18nService.getCurrentLanguage();
+    isOpenLang = signal<boolean>(false);
+    isScrolled = signal<boolean>(false);
+    isMobile = signal<boolean>(window.innerWidth < 1024);
+    isMenuOpen = signal<boolean>(false);
     readonly logoFullPath = 'assets/logo/KNUDEVLogoFull.svg';
     readonly logoMiniPath = 'assets/logo/KNUDEVLogoMini.svg';
     readonly arrowDownPath = 'assets/icon/system/arrowDown.svg';
-    readonly avatarNotFoundPath = 'assets/icon/avatarNotFound.svg';
+    readonly defaultAvatarPath = 'assets/icon/defaultAvatar.svg';
     readonly menuIconPath = 'assets/icon/system/menu.svg';
     readonly closeIconPath = 'assets/icon/system/close.svg';
 
@@ -46,48 +47,35 @@ export class HeaderComponent {
     onWindowScroll() {
         const scrollPosition =
             window.scrollY || document.documentElement.scrollTop;
-        this.isScrolled = scrollPosition > 50;
+        this.isScrolled.set(scrollPosition > 50);
     }
 
     @HostListener('document:click', ['$event'])
     onDocumentClick(event: MouseEvent) {
         const target = event.target as HTMLElement;
         if (!target.closest('.language-selector')) {
-            this.isOpenLang = false;
+            this.isOpenLang.set(false);
         }
     }
 
     @HostListener('window:resize', ['$event'])
     onResize() {
-        this.isMobile = window.innerWidth < 1440;
-        if (!this.isMobile) {
-            this.isMenuOpen = false;
+        this.isMobile.set(window.innerWidth < 1440);
+        if (!this.isMobile()) {
+            this.isMenuOpen.set(false);
         }
     }
     toggleMenu() {
-        this.isMenuOpen = !this.isMenuOpen;
+        this.isMenuOpen.update((value) => !value);
     }
+
     toggleDropdownLang() {
-        this.isOpenLang = !this.isOpenLang;
+        this.isOpenLang.update((value) => !value);
     }
 
     selectLanguage(code: string) {
         this.languageSwitcher.switchLang(code as any);
-        this.isOpenLang = false;
-    }
-
-    getCurrentLanguageFlag() {
-        return this.languageSwitcher
-            .supportedLanguages()
-            .find((lang) => lang.code === this.languageSwitcher.currentLang())
-            ?.imgMiniPath;
-    }
-
-    getCurrentLanguageName() {
-        return this.languageSwitcher
-            .supportedLanguages()
-            .find((lang) => lang.code === this.languageSwitcher.currentLang())
-            ?.name;
+        this.isOpenLang.set(false);
     }
 
     isAuthPage(): boolean {
@@ -96,24 +84,21 @@ export class HeaderComponent {
 
     constructor() {
         const langChange$ = this.translate.onLangChange.pipe(
-            startWith({ lang: this.translate.currentLang } as LangChangeEvent),
+            startWith({ lang: this.translate.currentLang } as LangChangeEvent)
         );
 
         const loadTranslations$ = langChange$.pipe(
             switchMap((event) =>
-                this.i18nService.loadComponentTranslations(
-                    'header',
-                    event.lang,
-                ),
-            ),
+                this.i18nService.loadComponentTranslations('header', event.lang)
+            )
         );
 
         const menuTranslations$ = loadTranslations$.pipe(
-            switchMap(() => this.translate.get([MENU_TRANSLATIONS])),
+            switchMap(() => this.translate.get([MENU_TRANSLATIONS]))
         );
 
         this.menu$ = menuTranslations$.pipe(
-            map((translations) => translations[MENU_TRANSLATIONS] || []),
+            map((translations) => translations[MENU_TRANSLATIONS] || [])
         );
     }
 }
