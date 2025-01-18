35f165d5eba71a4ae15afe70f148a887fb863e35
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-01-18 20:52:14 +0200

    Bug fix

--- src/app/services/department.services.ts
+++ src/app/services/department.services.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError, timeout } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from '../../environments/environment.development';
 import { Department, Specialty } from '../pages/auth/register/register.model';
 
@@ -8,13 +8,10 @@ import { Department, Specialty } from '../pages/auth/register/register.model';
     providedIn: 'root',
 })
 export class DepartmentService {
-    private readonly TIMEOUT = 1000;
-
     constructor(private readonly http: HttpClient) {}
 
     getDepartments(): Observable<Department[]> {
         return this.http.get<Department[]>(environment.apiDepartmentsUrl).pipe(
-            timeout(this.TIMEOUT),
             catchError((error: HttpErrorResponse) => {
                 return throwError(() => error);
             })
@@ -27,7 +24,6 @@ export class DepartmentService {
                 `${environment.apiDepartmentsUrl}/${departmentId}/specialties`
             )
             .pipe(
-                timeout(this.TIMEOUT),
                 catchError((error: HttpErrorResponse) => {
                     return throwError(() => error);
                 })
