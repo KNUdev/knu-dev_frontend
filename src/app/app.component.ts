4df5ce0ed69ca4de021f084a917c3bd6fbf555a7
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-02-01 18:32:27 +0200

    Add loading-screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,32 @@ import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoadingScreenComponent } from './components/loading/loading.component';
+import { LoadingService } from './services/loading.services';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent],
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        FooterComponent,
+        LoadingScreenComponent,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+    constructor(protected loadingService: LoadingService) {}
+
+    ngOnInit() {
+        // Hide loading screen after resources are loaded
+        window.addEventListener('load', () => {
+            setTimeout(() => {
+                this.loadingService.hide();
+            }, 500); // Add small delay for smoother transition
+        });
+    }
     private router = inject(Router);
 
     isAuthPage(): boolean {
---
+++ src/app/components/loading/load-anim/load-anim.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'load-anim',
+    imports: [CommonModule],
+    templateUrl: './load-anim.component.html',
+    styleUrl: './load-anim.component.scss',
+})
+export class LoadAnim {}
