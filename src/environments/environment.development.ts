6561e1eef4766bcef76f18c79112d0d99bc44f6d
DenysLeonenko <ds.leonenko@gmail.com>
2025-03-04 16:04:28 +0200

    [TESTENV-CONFIGURATION]changed some files for deploying

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,8 @@
-const apiBaseUrl = 'http://localhost:5001';
+// TODO uncomment for local developing
+// const apiBaseUrl = 'http://localhost:5001';
+
+// TODO NOT delete we use it for test-env
+const apiBaseUrl = 'https://knu-devbackend-production.up.railway.app';
 
 export const environment = {
     production: false,
