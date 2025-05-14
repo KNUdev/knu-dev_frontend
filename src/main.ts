2d6ec2f4f86b934ac2afc6d190d2dff35a382f3f
Shyshkin Andrey <shyshkinandrey06@gmail.com>
2025-05-14 19:49:38 +0300

    Server fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import {bootstrapApplication} from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 import {
     PreloadAllModules,
     provideRouter,
@@ -6,34 +9,32 @@ import {
     withPreloading,
     withViewTransitions,
 } from '@angular/router';
-import {LOCALE_ID} from '@angular/core';
-import {AppComponent} from './app/app.component';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
-import {provideTranslateService} from '@ngx-translate/core';
-import {routes} from './app/services/app.routes';
-import {registerLocaleData} from '@angular/common';
+import { provideTranslateService } from '@ngx-translate/core';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/services/app.routes';
 
-import localeUk from '@angular/common/locales/uk';
 import localeEn from '@angular/common/locales/en';
-import {notFoundInterceptor} from './app/pages/error/404/404.interceptor';
+import localeUk from '@angular/common/locales/uk';
+import { ngrokHeaderInterceptor } from './app/interceptors/ngrok-header.interceptor';
+import { notFoundInterceptor } from './app/pages/error/404/404.interceptor';
 
 const LANG_TO_LOCALE: { [key: string]: string } = {
     uk: 'uk-UA',
     en: 'en-UK',
 };
 
-registerLocaleData(localeUk, LANG_TO_LOCALE["uk"]);
-registerLocaleData(localeEn, LANG_TO_LOCALE["en"]);
+registerLocaleData(localeUk, LANG_TO_LOCALE['uk']);
+registerLocaleData(localeEn, LANG_TO_LOCALE['en']);
 
 function localeIdFactory(): string {
     const savedLang = localStorage.getItem('selectedLanguage') || 'uk';
-    return LANG_TO_LOCALE[savedLang] || LANG_TO_LOCALE["en"];
+    return LANG_TO_LOCALE[savedLang] || LANG_TO_LOCALE['en'];
 }
 
 bootstrapApplication(AppComponent, {
     providers: [
         provideHttpClient(
-            withInterceptors([notFoundInterceptor])
+            withInterceptors([notFoundInterceptor, ngrokHeaderInterceptor])
         ),
         provideTranslateService(),
         provideRouter(
@@ -44,7 +45,7 @@ bootstrapApplication(AppComponent, {
         ),
         {
             provide: LOCALE_ID,
-            useFactory: localeIdFactory
+            useFactory: localeIdFactory,
         },
-    ]
+    ],
 }).catch((err) => console.error(err));
